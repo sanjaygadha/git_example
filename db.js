6814bd499d0c92b7f0332431376d1c62d9479357
sanjaygadha <sanjaygadha21200@gamil.com>
2025-01-04 16:57:59 +0530

    env example

---
+++ db.js
@@ -0,0 +1,6 @@
+require("dotenv").config()
+
+username=process.env.db_name
+password=process.env.password
+
+console.log(username,password)
---
+++ index.js
@@ -0,0 +1,35 @@
+// const bcrypt=require("bcrypt")
+
+// password="secret123"
+// let hashed_password
+// const encrypt=async()=>{
+//     const salt=await bcrypt.genSalt()
+//     console.log(salt)
+
+//     hashed_password= await bcrypt.hash("secret123",salt)//4-31  saltrounds or costfactor
+//     console.log("password",hashed_password)
+
+//     const times=  bcrypt.getRounds(hashed_password)
+//     console.log(times)
+
+//     const original=await bcrypt.compare(password,hashed_password)
+//     console.log(original)
+
+// }
+// encrypt()
+
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(3)
+console.log(3)
+console.log(3)
+console.log(3)
+console.log(3)
+console.log(3)
+console.log(3)
+console.log(3)
+console.log(3)
+console.log(3)
+console.log("hello")
